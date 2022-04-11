edc7ceed0d71d6e67802923c35b88f4561867e1e
Martins Zeltins <martinszeltins@gmail.com>
2022-04-11 21:19:36 +0300

    Cleanup

--- app/client/tailwind.config.js
+++ app/client/tailwind.config.js
@@ -6,22 +6,7 @@ module.exports = {
 
     theme: {
         extend: {
-            colors: {
-                "neutral-750": "#323232"
-            },
-
-            fontFamily: {
-                "pseudo-apl": "PseudoApl",
-            },
-
-            backgroundImage: {
-                "field": "url('/src/assets/img/bg/field.jpg')",
-            },
-
-            boxShadow: {
-                "beneath": "0 1px #dadada",
-                "beneath-darker": "0 1px #c4c4c4",
-            },
+            
         },
     },
 
