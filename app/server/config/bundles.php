ed52f543155108d874d44856d9acbf9718f207b4
Martins Zeltins <martinszeltins@gmail.com>
2022-04-12 20:32:55 +0300

    Installed API platform

--- app/server/config/bundles.php
+++ app/server/config/bundles.php
@@ -15,4 +15,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
+    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
