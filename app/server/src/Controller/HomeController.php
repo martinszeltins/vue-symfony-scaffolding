5b1c9f17210d16119a6188aba9b6f1c173fe5ee8
Martins Zeltins <martinszeltins@gmail.com>
2022-04-12 20:13:44 +0300

    Symfony setup

--- app/server/src/Controller/HomeController.php
+++ app/server/src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,8 +12,14 @@ class HomeController extends AbstractController
     /**
      * @Route("/")
      */
-    public function index()
+    public function index(UserRepository $user): Response
     {
-        return new Response('home!');
+        $myUser = $user->find(1);
+
+        return $this->json([
+            'message' => 'Welcome to the Home controller!',
+            'path' => 'src/Controller/HomeController.php',
+            'username' => $myUser->getUsername(),
+        ]);
     }
 }
