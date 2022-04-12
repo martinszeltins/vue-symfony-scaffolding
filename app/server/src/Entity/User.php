bd4e7acb1f2f1d326d766fa24efa932774e4e6b8
Martins Zeltins <martinszeltins@gmail.com>
2022-04-12 20:52:49 +0300

    Added API resource to User

--- app/server/src/Entity/User.php
+++ app/server/src/Entity/User.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use App\Repository\UserRepository;
+use ApiPlatform\Core\Annotation\ApiResource;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
