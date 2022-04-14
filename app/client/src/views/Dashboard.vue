09e2f08025b847a70463a16cc3ea2e2c57a9f15a
Martins Zeltins <martinszeltins@gmail.com>
2022-04-14 23:59:31 +0300

    Dashboard

--- app/client/src/views/Dashboard.vue
+++ app/client/src/views/Dashboard.vue
@@ -5,7 +5,7 @@
         <div class="flex flex-col flex-1">
             <main class="h-full pb-16 overflow-y-auto">
                 <div class="container mx-auto grid">
-                    <h1 class="text-center mt-8 mb-4 text-3xl font-semibold text-gray-600 dark:text-gray-300">
+                    <h1 class="text-center mt-8 mb-4 text-3xl font-semibold text-gray-800 dark:text-gray-300">
                         Dashboard
                     </h1>
                 </div>
