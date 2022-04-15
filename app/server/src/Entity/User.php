094e44bfd25ebd6eb7e4c8047529c230f7155746
Martins Zeltins <martinszeltins@gmail.com>
2022-04-15 22:41:24 +0300

    Cleanup

--- app/server/src/Entity/User.php
+++ app/server/src/Entity/User.php
@@ -97,7 +97,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this;
     }
 
-
     public function eraseCredentials()
     {
         // If you store any temporary, sensitive data on the user, clear it here
