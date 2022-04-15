73daf69f3fd6038759059c0565deb39ec0d4bc50
Martins Zeltins <martinszeltins@gmail.com>
2022-04-15 22:24:09 +0300

    Added modal component

--- app/client/src/services/useGlobalComponents.ts
+++ app/client/src/services/useGlobalComponents.ts
@@ -1,11 +1,13 @@
 import Menu from 'primevue/menu'
 import Button from 'primevue/button'
+import Dialog from 'primevue/dialog'
 import Password from 'primevue/password'
 import InputText from 'primevue/inputtext'
 
 export function useGlobalComponents() {
     const registerGlobalComponents = (app) => {
         app.component('AppMenu', Menu)
+        app.component('AppModal', Dialog)
         app.component('AppButton', Button)
         app.component('AppInput', InputText)
         app.component('AppPassword', Password)
