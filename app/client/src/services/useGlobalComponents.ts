99a696681f1f471b1fc2bf30b86e859011caf696
Martins Zeltins <martinszeltins@gmail.com>
2022-04-15 09:50:28 +0300

    Forms example in dashboard

--- app/client/src/services/useGlobalComponents.ts
+++ app/client/src/services/useGlobalComponents.ts
@@ -1,10 +1,12 @@
 import Menu from 'primevue/menu'
 import Button from 'primevue/button'
+import InputText from 'primevue/inputtext'
 
 export function useGlobalComponents() {
     const registerGlobalComponents = (app) => {
         app.component('AppMenu', Menu)
         app.component('AppButton', Button)
+        app.component('AppInput', InputText)
     }
 
     return { registerGlobalComponents }
--- app/client/src/views/admin/Dashboard.vue
+++ app/client/src/views/admin/Dashboard.vue
@@ -1,5 +1,15 @@
 <template>
-    <h1 class="text-center mt-8 mb-4 text-3xl font-semibold text-gray-800 dark:text-gray-300">
-        Dashboard
-    </h1>
+    <h2 class="my-6 text-2xl font-semibold text-gray-700">
+        Forms
+    </h2>
+
+    <div class="p-6 bg-white rounded-lg shadow-md space-y-4">
+        <div v-for="n in 20" class="flex flex-col gap-4">
+            <div class="text-gray-700 text-base font-semibold">
+                Name
+            </div>
+
+            <AppInput type="text" />
+        </div>
+    </div>
 </template>
